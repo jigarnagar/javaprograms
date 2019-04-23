ab1472579b9f1204c3b7458ec52b2a0fdb6e5aa6
jigarnagar <jigarnagar112@gmail.com>
2019-04-24 02:09:55 +0530

    starting

---
+++ javahelper/src/main/java/com/javaprogram/practice/scanner/ScannerPractice.java
@@ -0,0 +1,21 @@
+package com.javaprogram.practice.scanner;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Jigar Nagar
+ *
+ */
+public class ScannerPractice {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		// Read the Integer Value
+		int i = scanner.nextInt();
+
+		System.out.println("i " + i);
+	}
+
+}
